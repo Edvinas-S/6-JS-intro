d67b58eca778de7e59aadc2a8612cb6a3c68b037
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-04-28 12:46:26 +0300

    Initial commit and variables

---
+++ js/variables.js
@@ -0,0 +1,44 @@
+/* JavaScript mini uzduotys
+---------------------------------------------------------------*/
+
+// Kintamuju inicijavimas //
+
+// 1 //
+const pirmasSkaicius = 10;
+console.log(pirmasSkaicius);
+
+const antrasSkaicius = 15;
+console.log ( antrasSkaicius );
+
+const treciasSkaicius = 25;
+console.log( treciasSkaicius );
+
+// 2 //
+const pirmasTekstas = `Labas`;
+console.log( pirmasTekstas );
+
+const antrasTekstas = 'as';
+console.log( antrasTekstas );
+
+const treciasTekstas = "krabas";
+console.log(treciasTekstas);
+
+// 3 //
+const sarasoTipoSkaiciai1 = [10, 10, 10, 10, 10];
+console.log(sarasoTipoSkaiciai1);
+
+const sarasoTipoSkaiciai2=[15, 20, 25, 30, 35];
+console.log(sarasoTipoSkaiciai2);
+
+const sarasoTipoSkaiciai3 = [50, 45, 40, 35, 30];
+console.log(sarasoTipoSkaiciai3);
+
+// 4 //
+const sarasoTipoTekstas1 = [`Labas`,`mano`,`vardas`,`krabu`,`krabas`];
+console.log(sarasoTipoTekstas1);
+
+const sarasoTipoTekstas2=["Krabu","krabas","vardas","mano"," - labas"];
+console.log(sarasoTipoTekstas2);
+
+const sarasoTipoTekstas3 = ['vienas','du','trys','keturi','penki'];
+console.log(sarasoTipoTekstas3);
